9909a6a772a13e998b198fd707b5a4351661cb27
Peter Heilbo Ratgen <peter@pratgen.dk>
2021-04-14 14:57:44 +0200

    Add implementation of interface

--- OSGiGame/Common/src/main/java/dk/group6/common/data/entityparts/LifePart.java
+++ OSGiGame/Common/src/main/java/dk/group6/common/data/entityparts/LifePart.java
@@ -1,5 +1,12 @@
 package dk.group6.common.data.entityparts;
 
-public class LifePart {
+import dk.group6.common.data.Entity;
+import dk.group6.common.data.GameData;
+
+public class LifePart implements EntityPart {
+
+    public void process(GameData gameData, Entity entity) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 
 }
--- OSGiGame/Common/src/main/java/dk/group6/common/data/entityparts/MovingPart.java
+++ OSGiGame/Common/src/main/java/dk/group6/common/data/entityparts/MovingPart.java
@@ -1,5 +1,12 @@
 package dk.group6.common.data.entityparts;
 
-public class MovingPart {
+import dk.group6.common.data.Entity;
+import dk.group6.common.data.GameData;
+
+public class MovingPart implements EntityPart {
+
+    public void process(GameData gameData, Entity entity) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 
 }
