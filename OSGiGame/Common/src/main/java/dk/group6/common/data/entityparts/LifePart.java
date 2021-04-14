4c6658831033e8814d90d4d3d412b63f0eab9093
joakimbetzer <43139396+joakimbetzer@users.noreply.github.com>
2021-04-14 15:29:54 +0200

    Updated LifePart

--- OSGiGame/Common/src/main/java/dk/group6/common/data/entityparts/LifePart.java
+++ OSGiGame/Common/src/main/java/dk/group6/common/data/entityparts/LifePart.java
@@ -3,10 +3,40 @@ package dk.group6.common.data.entityparts;
 import dk.group6.common.data.Entity;
 import dk.group6.common.data.GameData;
 
-public class LifePart implements EntityPart {
+public class LifePart implements EntityPart{
 
+    private boolean dead = false;
+    private int hitPoints;
+    private boolean struck = false;
+    
+    public LifePart (int hitPoints) {
+        this.hitPoints = hitPoints;
+    }
+    
+    public int getHitPoints() {
+        return hitPoints;
+    }
+    
+    public void setLife (int hitPoints) {
+        this.hitPoints = hitPoints;
+    }
+    
+    public boolean isHit() {
+        return struck;
+    }
+    
+    public boolean isDead() {
+        return dead;
+    }
+    
     public void process(GameData gameData, Entity entity) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if (struck) {
+            hitPoints = -1;
+            struck = false;
+        }
+        if (hitPoints <= 0) {
+            dead = true;
+        }
     }
 
 }
