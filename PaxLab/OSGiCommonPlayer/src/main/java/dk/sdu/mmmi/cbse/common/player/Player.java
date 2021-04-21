60c1bad1b4bcbbffa867e73dcb8bc0741dc222b1
Yonus <moraf19@student.sdu.dk>
2021-04-21 14:13:53 +0200

    PaxLab
    Reset

---
+++ PaxLab/OSGiCommonPlayer/src/main/java/dk/sdu/mmmi/cbse/common/player/Player.java
@@ -0,0 +1,10 @@
+package dk.sdu.mmmi.cbse.common.player;
+
+import dk.sdu.mmmi.cbse.common.data.Entity;
+
+/**
+ *
+ * @author corfixen
+ */
+public class Player extends Entity {
+}
---
+++ PaxLab/OSGiPlayer/src/main/java/dk/sdu/mmmi/cbse/osgiplayer/PlayerPlugin.java
@@ -0,0 +1,50 @@
+package dk.sdu.mmmi.cbse.osgiplayer;
+
+import dk.sdu.mmmi.cbse.common.data.Entity;
+import dk.sdu.mmmi.cbse.common.data.GameData;
+import dk.sdu.mmmi.cbse.common.data.World;
+import dk.sdu.mmmi.cbse.common.data.entityparts.LifePart;
+import dk.sdu.mmmi.cbse.common.data.entityparts.MovingPart;
+import dk.sdu.mmmi.cbse.common.data.entityparts.PositionPart;
+import dk.sdu.mmmi.cbse.common.player.Player;
+import dk.sdu.mmmi.cbse.common.services.IGamePluginService;
+
+public class PlayerPlugin implements IGamePluginService {
+    private String playerID;
+
+    public PlayerPlugin() {
+    }
+
+    @Override
+    public void start(GameData gameData, World world) {
+        // Add entities to the world
+        Entity player = createPlayerShip(gameData);
+        playerID = world.addEntity(player);
+        
+    }
+
+    private Entity createPlayerShip(GameData gameData) {
+        Entity playerShip = new Player();
+
+        float deacceleration = 10;
+        float acceleration = 200;
+        float maxSpeed = 300;
+        float rotationSpeed = 5;
+        float x = gameData.getDisplayWidth() / 3;
+        float y = gameData.getDisplayHeight() / 3;
+        float radians = 3.1415f / 2;
+        playerShip.add(new LifePart(3));
+        playerShip.setRadius(4);
+        playerShip.add(new MovingPart(deacceleration, acceleration, maxSpeed, rotationSpeed));
+        playerShip.add(new PositionPart(x, y, radians));
+        
+        return playerShip;
+    }
+
+    @Override
+    public void stop(GameData gameData, World world) {
+        // Remove entities
+        world.removeEntity(playerID);
+    }
+
+}
