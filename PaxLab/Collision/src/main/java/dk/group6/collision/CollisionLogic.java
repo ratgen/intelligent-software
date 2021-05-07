e40dde4439168647a96080dc9cbe77379061fe67
Yonus <moraf19@student.sdu.dk>
2021-05-08 01:17:57 +0200

    Created Collision component (no content yet)

---
+++ PaxLab/Collision/src/main/java/dk/group6/collision/CollisionLogic.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dk.group6.collision;
+
+import dk.group6.common.data.GameData;
+import dk.group6.common.data.World;
+import dk.group6.common.services.IPostEntityProcessingService;
+
+/**
+ *
+ * @author Yonus
+ */
+public class CollisionLogic implements IPostEntityProcessingService {
+
+    @Override
+    public void process(GameData gameData, World world) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
