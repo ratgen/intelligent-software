9eaf5ec2e15f1bec7766dc2d52510b899fd04ba3
Azzi <ArminAlicajic1324@hotmail.com>
2021-05-10 12:17:45 +0200

    Setup for implementing shoot

--- PaxLab/Player/src/main/java/dk/group6/player/PlayerProcessor.java
+++ PaxLab/Player/src/main/java/dk/group6/player/PlayerProcessor.java
@@ -7,6 +7,7 @@ import dk.group6.common.data.World;
 import dk.group6.common.data.entityparts.MovingPart;
 import dk.group6.common.data.entityparts.PositionPart;
 import dk.group6.common.data.entityparts.SpritePart;
+import dk.group6.common.data.entityparts.WeaponPart;
 import dk.group6.common.player.Player;
 import dk.group6.common.services.IEntityProcessingService;
 
@@ -22,15 +23,18 @@ public class PlayerProcessor implements IEntityProcessingService {
             PositionPart positionPart = entity.getPart(PositionPart.class);
             MovingPart movingPart = entity.getPart(MovingPart.class);
             SpritePart spritePart = entity.getPart(SpritePart.class);
+            WeaponPart weaponPart = entity.getPart(WeaponPart.class);
 
             movingPart.setLeft(gameData.getKeys().isDown(GameKeys.LEFT));
             movingPart.setRight(gameData.getKeys().isDown(GameKeys.RIGHT));
             movingPart.setUp(gameData.getKeys().isDown(GameKeys.UP));
             movingPart.setDown(gameData.getKeys().isDown(GameKeys.DOWN));
+            weaponPart.setEnter(gameData.getKeys().isDown(GameKeys.ENTER));
             
             movingPart.process(gameData, entity);
             positionPart.process(gameData, entity);  
             spritePart.process(gameData, entity);
+            weaponPart.process(gameData, entity);
         }
     }
 }
