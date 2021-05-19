540e5ca866fe59fc5b9aedf6f7ed4fbd38a37a62
joakimbetzer <43139396+joakimbetzer@users.noreply.github.com>
2021-05-19 17:00:31 +0200

    Test start

---
+++ PaxLab/Player/src/test/java/PlayerTest.java
@@ -0,0 +1,56 @@
+
+import dk.group6.common.data.Entity;
+import dk.group6.common.data.GameData;
+import dk.group6.common.data.World;
+import dk.group6.common.data.entityparts.PositionPart;
+import dk.group6.common.player.Player;
+import dk.group6.player.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.osgi.framework.FrameworkUtil;
+
+/**
+ *
+ * @author joaki
+ */
+public class PlayerTest {
+    
+    public PlayerTest() {
+    }
+
+    @org.junit.jupiter.api.BeforeAll
+    public static void setUpClass() throws Exception {
+    }
+
+    @org.junit.jupiter.api.AfterAll
+    public static void tearDownClass() throws Exception {
+    }
+
+    @org.junit.jupiter.api.BeforeEach
+    public void setUp() throws Exception {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    public void tearDown() throws Exception {
+    }
+
+    
+     @Test
+     public void playerTest() {
+        GameData gameData = new GameData();
+        World world = new World();
+        
+        PlayerPlugin plugin = new PlayerPlugin();
+        plugin.start(gameData, world);
+        
+        Entity player = world.getEntities().iterator().next();
+        
+        PositionPart positionPart = player.getPart(PositionPart.class);
+        assertEquals(positionPart.getX(), 360);
+        assertEquals(positionPart.getY(), 540);
+     }
+}
