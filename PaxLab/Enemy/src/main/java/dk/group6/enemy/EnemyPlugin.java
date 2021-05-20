ebcd3126090db20f527926e4f94985fc34cde51d
Yonus <moraf19@student.sdu.dk>
2021-05-20 17:08:38 +0200

    Different spawnpoints for every enemy

--- PaxLab/Enemy/src/main/java/dk/group6/enemy/EnemyPlugin.java
+++ PaxLab/Enemy/src/main/java/dk/group6/enemy/EnemyPlugin.java
@@ -9,27 +9,41 @@ import dk.group6.common.data.entityparts.PositionPart;
 import dk.group6.common.data.entityparts.SpritePart;
 import dk.group6.common.enemy.Enemy;
 import dk.group6.common.services.IGamePluginService;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class EnemyPlugin implements IGamePluginService {
-    int[][] spawnPoints = {{46, 629}, {100, 629}, {675, 495}, {46, 629}, {39, 523}, {55, 232}, {125, 211}};
-
+  
+    ArrayList<int[]> spawnPoints;
+    GameData gameData;
+    World world;
+    Random rand = new Random();
+    
     public EnemyPlugin() {
     }
-
+    
+    
+    
     @Override
     public void start(GameData gameData, World world) {
         // Add entities to the world
-
-        for (int i = 0; i < 4; i++) {
-            Entity enemy = createEnemy(gameData);
+        spawnPoints = new ArrayList<>();
+        loadSpawnPoints();
+        this.gameData = gameData;
+        this.world = world;
+        for (int i = 0; i < 4 ; i++) {
+            int random = rand.nextInt(-i);
+            int[] index = spawnPoints.get(random);
+            this.spawnPoints.remove(random);
+            Entity enemy = createEnemy(gameData, index);
             world.addEntity(enemy);
         }
     }
 
-    public Entity createEnemy(GameData gameData) {
-        Random rand = new Random();
-        int[] spawnPoint = spawnPoints[rand.nextInt(6)];
+    public Entity createEnemy(GameData gameData, int[] spawn) {
+        
+        //int[] spawnPoint = spawnPoints[rand.nextInt(6)];
 
         Entity enemy = new Enemy();
 
@@ -38,23 +52,24 @@ public class EnemyPlugin implements IGamePluginService {
 		MovingPart movingPart = new MovingPart();
         movingPart.setAcceleration(1);
         enemy.add(movingPart);
-        enemy.add(new PositionPart(spawnPoint[0], spawnPoint[1]));
+        enemy.add(new PositionPart(spawn[0], spawn[1]));
         enemy.add(new SpritePart("assets/enemy.png", this.getClass()));
 
         return enemy;
     }
 
  public void respawnEnemies(GameData gameData, World world){
-        
-        int level = 1;
-        
-        for (int i = 0; i < 4 + level; i++) {
-            Entity enemy = createEnemy(gameData);
-            world.addEntity(enemy);
-            level++;
-        }
-        
+        start(this.gameData, this.world);
     }
+ 
+ public void loadSpawnPoints(){
+     int[][] arr = {{46, 46}, {100, 629}, {675, 495}, {46, 629}, {39, 523}, {55, 232}, {125, 211}, {988, 46}, {510, 586}, {722, 404}};
+     for (int[] point : arr) {
+         this.spawnPoints.add(point);
+     }
+
+    }
+ 
     @Override
     public void stop(GameData gameData, World world) {
         // Remove entities
