36c790fc7a1f6674881bfb1f5ad07b361232cd91
Peter Heilbo Ratgen <peter@pratgen.dk>
2021-05-23 18:29:15 +0200

    clean up in A-star

--- PaxLab/AStar/src/main/java/dk/group6/astar/AStar.java
+++ PaxLab/AStar/src/main/java/dk/group6/astar/AStar.java
@@ -4,6 +4,7 @@ import dk.group6.common.ai.IPathFinderSPI;
 import dk.group6.common.data.Entity;
 import dk.group6.common.data.World;
 import dk.group6.common.data.entityparts.PositionPart;
+import java.util.Comparator;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
@@ -16,40 +17,43 @@ import java.util.TreeSet;
  * @author peter
  */
 public class AStar implements IPathFinderSPI  {
+  Node current = new Node();
+  protected HashSet<Node> explored = new HashSet<>();
 
-    Node current = new Node();
-	protected HashSet<Node> explored = new HashSet();
+  @Override
+  public LinkedList<Double> track(Entity from, Entity to, World world) {
+    explored.clear();
+    PositionPart positionFrom = from.getPart(PositionPart.class);
+    PositionPart positionTo = to.getPart(PositionPart.class);
+
+    if (to == null){
+      return null;
+    }
+
+    TreeSet<Node> fringe = new TreeSet<>(new CompareTotal());
+
+    Node goal = new Node(true, positionTo.getX(), positionTo.getY());
+    Node start = new Node(positionFrom.getX(), positionFrom.getY(), current.calcDistance(positionFrom.getX(), positionFrom.getY(), goal.getGoalX(), goal.getGoalY()));
+    fringe.add(start);
+
+    while (!fringe.isEmpty()) {
+      current = fringe.pollFirst();
+      if (current.getDistance() < 10) {
+        return current.getPath(current);
+      }
+      ArrayList<Node> ways = current.expand(current, goal, world, explored);
+      fringe.addAll(ways);
+    }
+
+    System.out.println("returning null");
+    return null;
+  }
+}
+
+class CompareTotal implements Comparator<Node> {
 
     @Override
-    public LinkedList<Double> track(Entity from, Entity to, World world) throws NullPointerException {
-		explored.clear();
-        PositionPart positionFrom = from.getPart(PositionPart.class);
-        PositionPart positionTo = to.getPart(PositionPart.class);
-
-		if (to == null){
-			return null;
-		}
-
-		TreeSet<Node> fringe = new TreeSet<>(new CompareTotal());
-
-        Node goal = new Node(true, positionTo.getX(), positionTo.getY());
-        Node start = new Node(positionFrom.getX(), positionFrom.getY(), current.calcDistance(positionFrom.getX(), positionFrom.getY(), goal.getGoalX(), goal.getGoalY()));
-        fringe.add(start);
-
-        while (!fringe.isEmpty()) {
-	
-			current = fringe.pollFirst();
-            
-            if (current.getDistance() < 10) {
-                return current.getPath(current);
-            }
-            
-            ArrayList<Node> ways = current.expand(current, goal, world, explored);
-			
-			fringe.addAll(ways);
-        }
-
-		System.out.println("returning null");
-		return null;
+    public int compare(Node n1, Node n2) {
+        return Double.compare(n1.getTotal(), n2.getTotal());
     }
 }
