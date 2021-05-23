a793c849b3cd884ffe43b048b072fbd406d548d4
Peter Heilbo Ratgen <peter@pratgen.dk>
2021-05-23 18:44:15 +0200

    remove unused imports in player

--- PaxLab/CommonPlayer/src/main/java/dk/group6/common/player/Player.java
+++ PaxLab/CommonPlayer/src/main/java/dk/group6/common/player/Player.java
@@ -1,15 +1,12 @@
 package dk.group6.common.player;
 
-import com.badlogic.gdx.files.FileHandle;
 import dk.group6.common.data.Entity;
 
 /**
  *
- * @author corfixen
  */
 public class Player extends Entity {
-    
     public Player() {
-        super();
+      super();
     }
 }
