fece98bd822b420f8de1a51ecde87b5dfaeb9a09
Peter Heilbo Ratgen <peter@pratgen.dk>
2021-05-23 18:42:54 +0200

    collision logic clean up

--- PaxLab/Collision/src/main/java/dk/group6/collision/CollisionLogic.java
+++ PaxLab/Collision/src/main/java/dk/group6/collision/CollisionLogic.java
@@ -25,146 +25,145 @@ import dk.group6.common.weapon.Weapon;
  * @author Yonus
  */
 public class CollisionLogic implements IPostEntityProcessingService {
+  TiledMapTileLayer sdf;
+  Rectangle r1, r2, r3;
+  GameData gameData;
 
-    TiledMapTileLayer sdf;
-    Rectangle r1, r2, r3;
-    GameData gameData;
+  @Override
+  public void process(GameData gameData, World world) {
+    entityCollision(world);
+    setWallDistance(world);
+    this.gameData = gameData; 
+  }
 
-    @Override
-    public void process(GameData gameData, World world) {
-        entityCollision(world);
-        setWallDistance(world);
-        this.gameData = gameData; 
-    }
+  public void entityCollision(World world) {
+    for (Entity entity : world.getEntities()) {
+      SpritePart spritePart = entity.getPart(SpritePart.class);
 
-    public void entityCollision(World world) {
-        for (Entity entity : world.getEntities()) {
-            SpritePart spritePart = entity.getPart(SpritePart.class);
+      Sprite entitySprite = spritePart.getSprite();
 
-			Sprite entitySprite = spritePart.getSprite();
+      if (entitySprite == null) {
+        continue;
+      }
 
-			if (entitySprite == null) {
-				continue;
-			}
+      r1 = new Rectangle(entitySprite.getBoundingRectangle());
 
-            r1 = new Rectangle(entitySprite.getBoundingRectangle());
+      for (Entity entity1 : world.getEntities()) {
+        SpritePart spritePart1 = entity1.getPart(SpritePart.class);
 
-            for (Entity entity1 : world.getEntities()) {
-                SpritePart spritePart1 = entity1.getPart(SpritePart.class);
+        Sprite entity1Sprite = spritePart1.getSprite();
 
-				Sprite entity1Sprite = spritePart1.getSprite();
+        if (entity1Sprite == null) {
+          continue;
+        }
 
-				if (entity1Sprite == null) {
-					continue;
-				}
+        r2 = new Rectangle(entity1Sprite.getBoundingRectangle());
+        // skips if entities are the same
+        if (entity.getID().equals(entity1.getID()) && entitySprite.equals(entity1Sprite)) {
+          continue;
+        }
+        if (r1.overlaps(r2)) {
+          // Player & Enemy
+          if ((entity1.getClass() == Player.class && entity.getClass() == Enemy.class) ||
+              (entity.getClass() == Player.class && entity1.getClass() == Enemy.class )) {
 
-                r2 = new Rectangle(entity1Sprite.getBoundingRectangle());
-                // skips if entities are the same
-                if (entity.getID().equals(entity1.getID()) && entitySprite.equals(entity1Sprite)) {
-                    continue;
-                }
-                if (r1.overlaps(r2)) {
-                    // Player & Enemy
-                    if ((entity1.getClass() == Player.class && entity.getClass() == Enemy.class) ||
-							(entity.getClass() == Player.class && entity1.getClass() == Enemy.class )) {
+            LifePart entityLife = entity.getPart(LifePart.class);
 
-                        LifePart entityLife = entity.getPart(LifePart.class);
-
-                        if (entityLife.getLife() > 0) {
-                            entityLife.setLife(entityLife.getLife() - 1);
-                            entityLife.setIsHit(true);
-                            break;
-                        } else if (entityLife.getLife() <= 0 && entitySprite != entity1Sprite) {
-                            if (entity.getClass().toString().contains("Player")) {
-                                gameData.setGameIsLost(true);
-                            }
-                            world.removeEntity(entity);
-                        }
-                    }
-                    // Enemy & Bullet
-                    if ((entity1.getClass() == Shot.class && entity.getClass() == Enemy.class) ||
-							(entity.getClass() == Shot.class && entity1.getClass() == Enemy.class )) {
-						// If bullet hits any entity, it looses a life
-						LifePart entityLife = entity.getPart(LifePart.class);
-
-						if (entityLife.getLife() > 0) {
-							entityLife.setLife(entityLife.getLife() - 1);
-							entityLife.setIsHit(true);
-							//remove enemy and bullet as well
-							world.removeEntity(entity);
-							world.removeEntity(entity1);
-
-							break;
-						}
-					}
-                }
+            if (entityLife.getLife() > 0) {
+              entityLife.setLife(entityLife.getLife() - 1);
+              entityLife.setIsHit(true);
+              break;
+            } else if (entityLife.getLife() <= 0 && entitySprite != entity1Sprite) {
+              if (entity.getClass().toString().contains("Player")) {
+                gameData.setGameIsLost(true);
+              }
+              world.removeEntity(entity);
             }
+              }
+          // Enemy & Bullet
+          if ((entity1.getClass() == Shot.class && entity.getClass() == Enemy.class) ||
+              (entity.getClass() == Shot.class && entity1.getClass() == Enemy.class )) {
+            // If bullet hits any entity, it looses a life
+            LifePart entityLife = entity.getPart(LifePart.class);
+
+            if (entityLife.getLife() > 0) {
+              entityLife.setLife(entityLife.getLife() - 1);
+              entityLife.setIsHit(true);
+              //remove enemy and bullet as well
+              world.removeEntity(entity);
+              world.removeEntity(entity1);
+
+              break;
+            }
+          }
         }
+      }
     }
+  }
 
-    public void setWallDistance(World world) {
-        for (Entity entity : world.getEntities()) {
-            MovingPart mp = entity.getPart(MovingPart.class);
-            SpritePart sp = entity.getPart(SpritePart.class);
+  public void setWallDistance(World world) {
+    for (Entity entity : world.getEntities()) {
+      MovingPart mp = entity.getPart(MovingPart.class);
+      SpritePart sp = entity.getPart(SpritePart.class);
 
-			if (sp.getSprite() == null || mp == null) {
-				continue;
-			}
+      if (sp.getSprite() == null || mp == null) {
+        continue;
+      }
 
-            int[] lb = sp.getSpriteLeftBottom();
-            int[] lt = sp.getSpriteLeftTop();
-            int[] rb = sp.getSpriteRightBottom();
-            int[] rt = sp.getSpriteRightTop();
-			int[] intarr = {1, 2, 3, 5, 8, 13, 21, 34, 55};
-				
-			for (int INT : intarr ) {
-				//left
-				if(check_left(lb, INT, world) && check_left(lt, INT, world)){
-					mp.setLeftDistance(55);
-				} else {
-					mp.setLeftDistance(INT);
-					break;
-				} 
-			}
-			for (int INT : intarr ) {
-				//up
-				if(check_up(lt, INT, world) && check_up(rt, INT, world)){
-					mp.setUpDistance(55);
-				} else {
-					mp.setUpDistance(INT);
-					break;
-				} 
-			}
-			for (int INT : intarr ) {
-				//right
-				if(check_right(rt, INT, world) && check_right(rb, INT, world)){
-					mp.setRightDistance(55);
-				} else {
-					mp.setRightDistance(INT);
-					break;
-				} 
-			}
-			for (int INT : intarr ) {
-				//down
-				if(check_down(rb, INT, world) && check_down(lb, INT, world)){
-					mp.setDownDistance(55);
-				} else {
-					mp.setDownDistance(INT);
-					break;
-				} 
-			}
-        }
+      int[] lb = sp.getSpriteLeftBottom();
+      int[] lt = sp.getSpriteLeftTop();
+      int[] rb = sp.getSpriteRightBottom();
+      int[] rt = sp.getSpriteRightTop();
+      int[] intarr = {1, 2, 3, 5, 8, 13, 21, 34, 55};
+
+      for (int INT : intarr ) {
+        //left
+        if(check_left(lb, INT, world) && check_left(lt, INT, world)){
+          mp.setLeftDistance(55);
+        } else {
+          mp.setLeftDistance(INT);
+          break;
+        } 
+      }
+      for (int INT : intarr ) {
+        //up
+        if(check_up(lt, INT, world) && check_up(rt, INT, world)){
+          mp.setUpDistance(55);
+        } else {
+          mp.setUpDistance(INT);
+          break;
+        } 
+      }
+      for (int INT : intarr ) {
+        //right
+        if(check_right(rt, INT, world) && check_right(rb, INT, world)){
+          mp.setRightDistance(55);
+        } else {
+          mp.setRightDistance(INT);
+          break;
+        } 
+      }
+      for (int INT : intarr ) {
+        //down
+        if(check_down(rb, INT, world) && check_down(lb, INT, world)){
+          mp.setDownDistance(55);
+        } else {
+          mp.setDownDistance(INT);
+          break;
+        } 
+      }
     }
-	private boolean check_left(int[] point, int offset, World world) {
-		return world.isValidCell(point[0] - offset , (point[1]) );
-	}
-	private boolean check_up(int[] point, int offset, World world) {
-		return world.isValidCell(point[0] , point[1] + offset );
-	}
-	private boolean check_right(int[] point, int offset, World world) {
-		return world.isValidCell( point[0] + offset , point[1] );
-	}
-	private boolean check_down(int[] point, int offset, World world) {
-		return world.isValidCell(point[0], point[1] - offset );
-	}
+  }
+  private boolean check_left(int[] point, int offset, World world) {
+    return world.isValidCell(point[0] - offset , (point[1]) );
+  }
+  private boolean check_up(int[] point, int offset, World world) {
+    return world.isValidCell(point[0] , point[1] + offset );
+  }
+  private boolean check_right(int[] point, int offset, World world) {
+    return world.isValidCell( point[0] + offset , point[1] );
+  }
+  private boolean check_down(int[] point, int offset, World world) {
+    return world.isValidCell(point[0], point[1] - offset );
+  }
 }
