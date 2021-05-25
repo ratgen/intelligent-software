1060db78178c2b6bce085ecb511d72949d9054eb
Peter Heilbo Ratgen <peter@pratgen.dk>
2021-05-26 01:26:12 +0200

    progress

--- PaxLab/Player/src/test/java/PlayerTest/PlayerTest.java
+++ PaxLab/Player/src/test/java/PlayerTest/PlayerTest.java
@@ -2,11 +2,16 @@ package PlayerTest;
 
 import javax.inject.Inject;
 import org.junit.After;
+import static org.junit.Assert.assertNotNull;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.ops4j.pax.exam.Option;
 import org.ops4j.pax.exam.*;
+import static org.ops4j.pax.exam.CoreOptions.bundle;
+import static org.ops4j.pax.exam.CoreOptions.junitBundles;
+import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
+import static org.ops4j.pax.exam.CoreOptions.options;
 import org.ops4j.pax.exam.junit.PaxExam;
 import org.ops4j.pax.exam.spi.container.ContainerConstants;
 import org.osgi.framework.BundleContext;
@@ -22,8 +27,18 @@ import org.osgi.framework.BundleException;
 //default reactor strategy is Per Method
 public class PlayerTest {
 
+
 	@Inject
 	private BundleContext bc;
+
+	@Configuration
+	public Object[] config(){
+		System.out.println (
+		"Working Directory = " + System.getProperty("user.dir"));
+		return options(
+				bundle("../runner/bundles/Player_1.0.0.SNAPSHOT.jar")
+		);
+	}
 
  
     @Before
@@ -35,6 +50,11 @@ public class PlayerTest {
     public void tearDown() {
        // ...
     }
+
+	@Test
+	public void assertTest() {
+		assertNotNull(bc);
+	}
  
     @Test
     public void PlayTest() throws BundleException {
