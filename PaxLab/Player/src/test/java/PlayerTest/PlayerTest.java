c1350f1cb801547df161d4825cc4bffe4931b022
Peter Heilbo Ratgen <peter@pratgen.dk>
2021-05-26 10:05:31 +0200

    removed another erroer

--- PaxLab/Player/src/test/java/PlayerTest/PlayerTest.java
+++ PaxLab/Player/src/test/java/PlayerTest/PlayerTest.java
@@ -6,14 +6,11 @@ import static org.junit.Assert.assertNotNull;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.ops4j.pax.exam.Option;
 import org.ops4j.pax.exam.*;
-import static org.ops4j.pax.exam.CoreOptions.bundle;
-import static org.ops4j.pax.exam.CoreOptions.junitBundles;
-import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
-import static org.ops4j.pax.exam.CoreOptions.options;
+import static org.ops4j.pax.exam.CoreOptions.*;
 import org.ops4j.pax.exam.junit.PaxExam;
-import org.ops4j.pax.exam.spi.container.ContainerConstants;
+import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
+import org.ops4j.pax.exam.spi.reactors.PerMethod;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.BundleException;
 
@@ -24,7 +21,8 @@ import org.osgi.framework.BundleException;
  * @author peter
  */
 @RunWith(PaxExam.class)
-//default reactor strategy is Per Method
+//restarts the framework for each test
+@ExamReactorStrategy(PerMethod.class)
 public class PlayerTest {
 
 
@@ -32,11 +30,12 @@ public class PlayerTest {
 	private BundleContext bc;
 
 	@Configuration
-	public Object[] config(){
-		System.out.println (
+	public Option[] config(){
+		System.out.println ( 
 		"Working Directory = " + System.getProperty("user.dir"));
-		return options(
-				bundle("../runner/bundles/Player_1.0.0.SNAPSHOT.jar")
+		return options(provision(
+				bundle("file://../runner/bundles/Player_1.0.0.SNAPSHOT.jar")),
+				junitBundles()
 		);
 	}
 
